cf872158c9c9280385efdd08fedaee94a10e5857
asemenov <semenov@goodwix.ru>
2015-03-19 19:18:53 +0300

    phantom driver

--- src/main/java/gunParser/EntryPoint.java
+++ src/main/java/gunParser/EntryPoint.java
@@ -1,12 +1,20 @@
 package gunParser;
 
 
+
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
+import java.util.Set;
 
 
 public class EntryPoint {
@@ -23,7 +31,26 @@ public class EntryPoint {
             //String login = "fgf@fsdf.ru";
             //String pass = "fgfgsdfd";
 
-            String mainUrl = "http://gunaccessorysupply.com";
+            String proxy = "88.150.136.178:3128";
+            Proxy p = new Proxy();
+            p.setHttpProxy(proxy).setFtpProxy(proxy).setSslProxy(proxy);
+            DesiredCapabilities cap = DesiredCapabilities.phantomjs();
+            cap.setCapability(CapabilityType.PROXY, p);
+            cap.setCapability("phantomjs.binary.path", "phantomjs.exe");
+            PhantomJSDriver driver = new PhantomJSDriver(cap);
+
+            driver.get("http://gunaccessorysupply.com");
+            WebElement element = driver.findElement(By.cssSelector("a[title=\"Log In\"]"));
+            element.click();
+            WebElement loginElem = driver.findElement(By.cssSelector("input[name=\"login[username]\"]"));
+            loginElem.sendKeys(login);
+            WebElement passElem = driver.findElement(By.cssSelector("input[name=\"login[password]\"]"));
+            passElem.sendKeys(pass);
+            WebElement submitElem = driver.findElement(By.cssSelector("button[id=\"send2\"]"));
+            submitElem.click();
+            Set<Cookie> cookieSet = driver.manage().getCookies();
+
+            /*String mainUrl = "http://gunaccessorysupply.com";
 
             Connection.Response mainResponse = PatientLoader.instance.createClientLikeConnection(mainUrl).execute();
             Document mainDoc = mainResponse.parse();
@@ -31,26 +58,29 @@ public class EntryPoint {
             Element loginElem = mainDoc.select("a[title=Log In]").first();
             String loginUrl = loginElem.attr("href");
             Map<String, String> cookies = mainResponse.cookies();
+            Thread.sleep(1300);
             Connection.Response loginPageResponse = PatientLoader.instance.createClientLikeConnection(loginUrl).cookies(cookies).execute();
             Document loginPageDoc = loginPageResponse.parse();
             Element loginFormElem = loginPageDoc.select("form[id=login-form]").first();
             String loginPostUrl = loginFormElem.attr("action");
             String formKey = loginPageDoc.select("input[name=form_key]").first().val();
 
+            Thread.sleep(1100);
+            PatientLoader.instance.setAuthCookies(loginPageResponse.cookies());
             Connection.Response loginPostResponse = PatientLoader.instance.createClientLikeConnection(loginPostUrl)
                     .cookies(loginPageResponse.cookies()).method(Connection.Method.POST)
-                    .data("login[username]", login).data("login[password]", pass).data("form_key", formKey)
-                    .referrer(loginPostUrl)
-                    .followRedirects(false).execute();
+                    .data("form_key1", formKey).data("login[username]", login).data("login[password]", pass).data("send","")
+                    .followRedirects(false).referrer(loginPostUrl).execute();
 
             PatientLoader.instance.setAuthCookies(loginPostResponse.cookies());
+                                        */
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
 
-            CategoryParser.instance.parseCategory(url);
+           // CategoryParser.instance.parseCategory(url);
 
     }
 }
--- src/main/java/gunParser/PatientLoader.java
+++ src/main/java/gunParser/PatientLoader.java
@@ -7,14 +7,17 @@ import org.jsoup.nodes.Document;
 import java.util.Map;
 import java.util.Random;
 
-public enum PatientLoader {
+public enum PatientLoader
+{
 
     instance;
 
     private Map<String, String> authCookies;
 
-    public Document loadUrl(String url) {
-        try {
+    public Document loadUrl(String url)
+    {
+        try
+        {
             Random rand = new Random();
             int randomNum = rand.nextInt(5) + 5;
             long randomDelay = 700 + randomNum * 150;
@@ -26,29 +29,50 @@ public enum PatientLoader {
             Document result = response.parse();
 
             return result;
-        } catch (Exception ex) {
+        }
+        catch (Exception ex)
+        {
             ex.printStackTrace();
             return null;
         }
     }
 
-    public void setAuthCookies(Map<String, String> cookies) {
+    public void setAuthCookies(Map<String, String> cookies)
+    {
         this.authCookies = cookies;
     }
 
-    public Connection createClientLikeConnection(String url){
+    public Connection createClientLikeConnection(String url)
+    {
         Connection connection = Jsoup.connect(url);
-        if(authCookies != null) {
+        if (authCookies != null)
+        {
             connection.cookies(authCookies);
         }
+
+        String headerCookie = null;
+
+        if (authCookies != null && authCookies.containsKey("frontend"))
+        {
+            headerCookie = "frontend=" + authCookies.get("frontend") + "; external_no_cache=1";
+        }
+
         connection.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
         connection.header("Accept-Encoding", "gzip, deflate, sdch");
         connection.header("Accept-Language", "en-US,en;q=0.8,ru;q=0.6");
-        connection.header("Cache-Control", "no-cache");
+        connection.header("Cache-Control", "max-age=0");
         connection.header("Connection", "keep-alive");
+        //connection.header("Content-Length", "103");
+        connection.header("Content-Type", "application/x-www-form-urlencoded");
+        if (headerCookie != null)
+        {
+            //connection.header("Cookie", headerCookie);
+        }
+
         connection.header("Host", "gunaccessorysupply.com");
+        connection.header("Origin", "http://gunaccessorysupply.com");
         connection.header("Pragma", "no-cache");
-        connection.header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36");
+        connection.header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36");
         return connection;
     }
 
